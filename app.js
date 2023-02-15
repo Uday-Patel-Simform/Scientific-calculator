3021c6819abaf70d3dae7cc0153cb2ba26fb7863
uday <uday.p@simformsolutions.com>
2023-02-15 09:37:07 +0530

    functional changes in app.js

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 let display = document.querySelector('h1');
 let buttons = document.querySelectorAll(".cal-btn");
-let memory=0;
-let data_memory=[];
+// init memory variables
+let memory = 0;
+let data_memory = [];
 buttons.forEach(button => {
     button.addEventListener('click', () => {
         if (button.textContent == 'X') {
@@ -13,33 +14,7 @@ buttons.forEach(button => {
             display.textContent += '-';
         }
         else if (button.textContent == '=') {
-            if(display.textContent.includes('^')){
-                let temp=display.textContent;
-                var count = temp.match(/\W/g).length;
-                if(count>1){
-                    display.textContent = 'Syntax Error!';
-                }
-                let base= display.textContent.slice(0,display.textContent.indexOf('^'));
-                let exponent= display.textContent.slice(display.textContent.indexOf('^')+1);
-                display.textContent = displayAns(Math.pow(base, exponent));
-            }else if(display.textContent.includes('%')){
-                let temp=display.textContent;
-                var count = temp.match(/\W/g).length;
-                if(count>1){
-                    display.textContent = 'Syntax Error!';
-                }
-                let n1= display.textContent.slice(0,display.textContent.indexOf('%'));
-                let n2= display.textContent.slice(display.textContent.indexOf('%')+1);
-                display.textContent = displayAns(n1%n2);
-            }
-            else{
-                try{
-                    display.textContent = displayAns( eval(display.textContent));
-                }
-                catch{
-                    display.textContent = 'Syntax Error!';
-                }
-            }
+            equals(display.textContent);
         }
         else if (button.textContent == 'xy') {
             display.textContent += '^';
@@ -48,16 +23,10 @@ buttons.forEach(button => {
             display.textContent = '';
         }
         else if (button.textContent == '⌫') {
-            if (display.textContent.length > 0) {
-                display.textContent = display.textContent.slice(0, -1);
-            }
+            back(display.textContent);
         }
         else if (button.textContent == '±') {
-            if (display.textContent.charAt(0) == '-') {
-                display.textContent = display.textContent.slice(1);
-            } else {
-                display.textContent = '-' + display.textContent;
-            }
+            pm_toggle(display.textContent);
         }
         else if (button.textContent == 'mod') {
             display.textContent += '%';
@@ -69,31 +38,27 @@ buttons.forEach(button => {
             display.textContent = displayAns(Math.log10(display.textContent));
         }
         else if (button.textContent == 'π') {
-            display.textContent = displayAns(display.textContent*Math.PI);
+            displayPI(display.textContent);
         }
         else if (button.textContent == '10x') {
-            display.textContent = displayAns(Math.pow(10,display.textContent));
+            display.textContent = displayAns(Math.pow(10, display.textContent));
         }
         else if (button.textContent == '√') {
             display.textContent = displayAns(Math.sqrt(display.textContent));
         }
         else if (button.textContent == 'n!') {
-            let fact=1;
-            for (let i = 1; i <= display.textContent; i++) {
-                fact *= i;
-            }
-            display.textContent = fact;
+            fact(display.textContent);
         }
         else if (button.textContent == 'x2') {
-            display.textContent = displayAns(Math.pow(display.textContent,2));
+            display.textContent = displayAns(Math.pow(display.textContent, 2));
         }
         else if (button.textContent == '1/x') {
-            display.textContent = displayAns(eval(1/display.textContent));
+            display.textContent = displayAns(eval(1 / display.textContent));
         }
         else if (button.textContent == '|x|') {
             display.textContent = displayAns(Math.abs(display.textContent));
         }
-        else if (button.textContent == 'exp'|| button.textContent == 'e') {
+        else if (button.textContent == 'exp' || button.textContent == 'e') {
             display.textContent = displayAns(Math.exp(display.textContent));
         }
         else if (button.textContent == 'sin') {
@@ -105,6 +70,15 @@ buttons.forEach(button => {
         else if (button.textContent == 'tan') {
             display.textContent = displayAns(Math.tan(display.textContent));
         }
+        else if (button.textContent == 'asin') {
+            display.textContent = displayAns(Math.asin(display.textContent));
+        }
+        else if (button.textContent == 'acos') {
+            display.textContent = displayAns(Math.acos(display.textContent));
+        }
+        else if (button.textContent == 'atan') {
+            display.textContent = displayAns(Math.atan(display.textContent));
+        }
         else if (button.textContent == '⌈x⌉') {
             display.textContent = displayAns(Math.ceil(display.textContent));
         }
@@ -115,19 +89,13 @@ buttons.forEach(button => {
             display.textContent = displayAns(Math.random());
         }
         else if (button.textContent == 'M+') {
-            memory += parseFloat(display.textContent);
-            data_memory.push(memory);
-            display.textContent = memory;
+            mAdd(display.textContent);
         }
         else if (button.textContent == 'M-') {
-            memory -= display.textContent;
-            data_memory.push(memory);
-            display.textContent = memory;
+            mSub(display.textContent);
         }
         else if (button.textContent == 'MC') {
-            memory = 0;
-            data_memory=[];
-            display.textContent = data_memory;
+            mClear();
         }
         else if (button.textContent == 'MR') {
             display.textContent = data_memory;
@@ -137,26 +105,26 @@ buttons.forEach(button => {
         }
         else if (button.textContent == 'DEG') {
             button.textContent = 'RAD';
-            display.textContent = display.textContent*180/Math.PI;
+            display.textContent = display.textContent * 180 / Math.PI;
         }
         else if (button.textContent == 'RAD') {
             button.textContent = 'DEG';
-            display.textContent = Math.PI*display.textContent/180;
+            display.textContent = Math.PI * display.textContent / 180;
         }
         else if (button.textContent == 'F-E') {
             display.textContent = parseFloat(display.textContent).toExponential();
         }
         else if (button.textContent == '2x') {
-            display.textContent = displayAns(Math.pow(2,display.textContent));
+            display.textContent = displayAns(Math.pow(2, display.textContent));
         }
         else if (button.textContent == '3x') {
-            display.textContent = displayAns(Math.pow(3,display.textContent));
+            display.textContent = displayAns(Math.pow(3, display.textContent));
         }
         else if (button.textContent == '∛x') {
-            display.textContent = displayAns(Math.pow(display.textContent,1/3));
+            display.textContent = displayAns(Math.pow(display.textContent, 1 / 3));
         }
         else if (button.textContent == 'x3') {
-            display.textContent = displayAns(Math.pow(display.textContent,3));
+            display.textContent = displayAns(Math.pow(display.textContent, 3));
         }
         else {
             display.textContent += button.textContent;
@@ -164,17 +132,105 @@ buttons.forEach(button => {
     });
 });
 
-function displayAns(ans) {
+
+// handling "=" operation
+const equals = (Content) => {
+    if (Content.includes('^')) {
+        let temp = Content;
+        var count = temp.match(/\W/g).length;
+        if (count > 1) {
+            display.textContent = 'Syntax Error!';
+        }
+        let base = Content.slice(0, Content.indexOf('^'));
+        let exponent = Content.slice(Content.indexOf('^') + 1);
+        display.textContent = displayAns(Math.pow(base, exponent));
+    } else if (Content.includes('%')) {
+        let temp = Content;
+        var count = temp.match(/\W/g).length;
+        if (count > 1) {
+            display.textContent = 'Syntax Error!';
+        }
+        let n1 = Content.slice(0, Content.indexOf('%'));
+        let n2 = Content.slice(Content.indexOf('%') + 1);
+        display.textContent = displayAns(n1 % n2);
+    }
+    else {
+        try {
+            display.textContent = displayAns(eval(Content));
+        }
+        catch {
+            display.textContent = 'Syntax Error!';
+        }
+    }
+}
+
+const displayAns = (ans) => {
     if (countDecimal(ans) > 10) {
         return ans.toFixed(10);
     } else {
         return ans;
     }
 }
-function countDecimal(ans) {
+const countDecimal = (ans) => {
     if (!Number.isInteger(ans)) {
         return ans.toString().split(".")[1].length;
     } else {
         return 0;
     }
+}
+
+// handling "⌫" operation
+const back = (content) => {
+    if (content.length > 0) {
+        content = content.slice(0, -1);
+    }
+    display.textContent = content;
+}
+
+// handling "+/-" toggle operation
+const pm_toggle = (content) => {
+    if (content.charAt(0) == '-') {
+        content = content.slice(1);
+    } else {
+        content = '-' + content;
+    }
+    display.textContent = content;
+}
+
+// handling "PI"
+const displayPI = (content) => {
+    console.log(typeof (c));
+    if (content == '') {
+        display.textContent = 3.14159265359;
+    }
+    display.textContent = displayAns(content * Math.PI);
+}
+
+// handling factorial function
+const fact = (content) => {
+    let fact = 1;
+    for (let i = 1; i <= content; i++) {
+        fact *= i;
+    }
+    content = fact;
+    display.textContent = content;
+}
+
+// Memory functions
+const mAdd = (content) => {
+    memory += parseFloat(content);
+    data_memory.push(memory);
+    display.textContent = memory;
+}
+
+const mSub = (content) => {
+    memory -= content;
+    data_memory.push(memory);
+    display.textContent = memory;
+}
+
+const mClear = () => {
+    memory = 0;
+    data_memory = [];
+    display.textContent = data_memory;
 }
